110f6bca14b208d1897f2c2fa0ea3125a3b86385
hosung-222 <gik11kr@naver.com>
2024-04-11 17:35:01 +0900

    feat: jwt 토큰 컨버터 구현
    JWT를 사용하여 사용자의 권한 정보를 추출하고, 해당 정보를 Spring Security의 인증 객체로 변환하는 로직 구현

--- gateway/src/main/java/shoppingmall/config/SecurityConfiguration.java
+++ gateway/src/main/java/shoppingmall/config/SecurityConfiguration.java
@@ -1,31 +1,70 @@
 package shoppingmall.config;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import java.util.stream.Collectors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
+@EnableWebFluxSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(
-        ServerHttpSecurity http
-    ) {
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-            .cors()
-            .and()
-            .csrf()
-            .disable()
-            .authorizeExchange()
-            .pathMatchers("/login/**", "/logout**")
-            .permitAll()
-            .anyExchange()
-            .authenticated()
-            .and()
-            .oauth2Login(); // to redirect to oauth2 login page.
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeExchange()
+                .pathMatchers("/login/**", "/logout**")
+                .permitAll()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2Login() // to redirect to oauth2 login page.
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
 
         return http.build();
+    }
+
+
+    Converter<Jwt, Mono<AbstractAuthenticationToken>> jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new GrantedAuthoritiesExtractor());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+
+    static class GrantedAuthoritiesExtractor implements Converter<Jwt, Collection<GrantedAuthority>> {
+        @SuppressWarnings("unchecked")
+        public Collection<GrantedAuthority> convert(Jwt jwt) {
+            final Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+            final Map<String, List<String>> clientRoles = (Map<String, List<String>>) resourceAccess.get("account");
+
+            return clientRoles.get("roles").stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+
+        }
     }
 }
